4023715a397f3d0e99cf06cfba2a9de038488299
cyk90290 <cyk87321@yahoo.com.tw>
2020-01-03 15:22:22 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/no15.java
@@ -0,0 +1,17 @@
+package com.example.lib;
+
+import java.util.Scanner;
+
+public class no15 {
+    public static void main(String[] args) {
+        System.out.println(add3());
+    }
+
+    static int add3() {
+        int addcount = 0;
+        for (int i = 0; i <= 100; i += 2) {
+            addcount += i;
+        }
+        return addcount;
+    }
+}
